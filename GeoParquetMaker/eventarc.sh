09fccd95883e9a4aad506528f721bd2545d26393
William Gislason <8203830+gislawill@users.noreply.github.com>
2023-09-17 21:42:50 -0700

    start of GeoParquet Maker

---
+++ GeoParquetMaker/eventarc.sh
@@ -0,0 +1,14 @@
+ENV=${1:?"Must set environment as first arg"}
+echo $ENV
+
+GEOPARQUET_SERVICE=${2:?"Must set GeoParquetMaker service"}
+
+gcloud eventarc triggers delete storage-events-trigger-$ENV \
+     --location=us-west1
+     
+gcloud eventarc triggers create storage-events-trigger-$ENV \
+     --location=us-west1 \
+     --destination-run-service=$GEOPARQUET_SERVICE \
+     --destination-run-region=us-west1 \
+     --event-filters="type=google.cloud.storage.object.v1.finalized" \
+     --service-account="geoparquet-maker@global-mangroves.iam.gserviceaccount.com"
