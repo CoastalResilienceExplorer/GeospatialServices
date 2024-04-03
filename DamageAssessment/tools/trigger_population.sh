56850fc0be1a67f7c6ae3c3a3bd96b2597c15620
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2024-04-03 11:48:32 -0700

    Updating to use remote triggers

--- DamageAssessment/tools/trigger_population.sh
+++ DamageAssessment/tools/trigger_population.sh
@@ -4,8 +4,8 @@ REMOTE_OUTPUT=${3:?"Must set remote path to flooding tiff.  This will be the dir
 LOCAL_OUTPUT=${4:?"Must set path to return GZIP TIFF."}
 
 
-# curl -X POST 'https://damages-staging-myzvqet7ua-uw.a.run.app/damage/dlr_guf/' \
-curl -X POST 'http://localhost:3001/population/GHSL_2020_100m/' \
+# curl -X POST 'http://localhost:3001/population/GHSL_2020_100m/' \
+curl -X POST 'https://damages-staging-myzvqet7ua-uw.a.run.app/damage/dlr_guf/' \
     -F "flooding=@$FLOODING" \
     -F "threshold=$THRESHOLD" \
     -F "output_to_gcs=$REMOTE_OUTPUT" > $LOCAL_OUTPUT
