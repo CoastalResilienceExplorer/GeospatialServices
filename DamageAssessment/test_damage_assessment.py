2482779de2b04438f4f051b0c034876505eae25e
Christopher J Lowrie <chlowrie@Christophers-MacBook-Pro.local>
2024-04-08 13:33:02 -0700

    Cleaning up API

--- DamageAssessment/test_damage_assessment.py
+++ DamageAssessment/test_damage_assessment.py
@@ -3,11 +3,13 @@ from population_assessment import main as population_assessment
 import subprocess
 import rioxarray as rxr
 import uuid
+import os
 
 BELIZE = "./data/belize_test_flooding.tiff"
+WRITE = int(os.getenv('TEST_WRITE', 0))
 
 
-def test_damages():
+def test_damages_no_filters():
     # This is intermediate processing to deal with non-rectilinear grids
     tmp_cog = f'/tmp/{str(uuid.uuid4())}.tiff'
     bashCommand = f"gdalwarp {BELIZE} {tmp_cog} -of COG"
@@ -18,6 +20,8 @@ def test_damages():
         print(line, flush=True)
     ds = rxr.open_rasterio(tmp_cog).isel(band=0)
     damages = damage_assessment(ds)
+    if WRITE:
+        damages.rio.to_raster('test_damages_no_filters.tiff')
 
 def test_population():
     import uuid
@@ -30,7 +34,25 @@ def test_population():
         if not line: break
         print(line, flush=True)
     ds = rxr.open_rasterio(tmp_cog).isel(band=0)
-    ds.rio.to_raster('./test1.tiff')
-    population_assessment(ds, 0.5)
+    pop = population_assessment(ds, 0.5)
+    if WRITE:
+        pop.rio.to_raster('test_population.tiff')
 
+def test_damages_with_filter():
+    import uuid
+    # This is intermediate processing to deal with non-rectilinear grids
+    tmp_cog = f'/tmp/{str(uuid.uuid4())}.tiff'
+    bashCommand = f"gdalwarp {BELIZE} {tmp_cog} -of COG"
+    process = subprocess.Popen(bashCommand.split(' '), stdout=subprocess.PIPE)
+    while True:
+        line = process.stdout.readline()
+        if not line: break
+        print(line, flush=True)
+    ds = rxr.open_rasterio(tmp_cog).isel(band=0)
+    x = damage_assessment(ds, 100, 10)
+    if WRITE:
+        x.rio.to_raster('./test_damages_with_filters.tiff')
+
+test_damages_no_filters()
 test_population()
+test_damages_with_filter()
--- DamageAssessment/tools/trigger.py
+++ DamageAssessment/tools/trigger.py
@@ -12,13 +12,25 @@ if __name__ == "__main__":
     parser.add_argument('-f', '--flooding', type=str, help='Path to the flooding file')
     parser.add_argument('-t', '--type', choices=['population', 'damages'], help='Type option damages of population')
     parser.add_argument('-p', '--project', type=str, help='Project name, ie Belize')
+    parser.add_argument('--output', type=str, help="Path to output resulting GeoTIFF")
+
     parser.add_argument('-i', '--id', required=False, type=str, help='''Identifier of the data.  Optional, used for posting to cloud storage.  
                         Can be nested.  
                         ie reefs/rp10/''')
     parser.add_argument('--threshold', required=False, default=0.5, type=float, help="The threshold to use, below which population will not be flooded")
-    parser.add_argument('--output', type=str, help="Path to output resulting GeoTIFF")
+    parser.add_argument('--window_size', type=int, default=0)
+    parser.add_argument('--population_min', type=int, default=5)
 
+    parser.add_argument('--local', action='store_true', default=False,  help="Run with local server")
     args = parser.parse_args()
+
+    if args.local:
+        HOST = 'http://localhost:3001'
+    else:
+        HOST = "https://damages-staging-myzvqet7ua-uw.a.run.app"
+    DAMAGES_ROUTE = os.path.join(HOST, "damage/dlr_guf/")
+    POPULATION_ROUTE = os.path.join(HOST, "population/GHSL_2020_100m/")
+
     if (args.type == "population"):
         data = {
             'threshold': args.threshold
@@ -29,6 +41,9 @@ if __name__ == "__main__":
         ENDPOINT = DAMAGES_ROUTE
     if args.id:
         data['output_to_gcs'] = os.path.join(args.project, args.id)
+        if args.window_size:
+            data['window_size'] = args.window_size
+            data['population_min'] = args.population_min
     
     files = {'flooding': open(args.flooding, 'rb')}
     response = requests.post(
